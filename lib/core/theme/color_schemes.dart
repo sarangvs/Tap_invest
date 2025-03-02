d628451df1c4ca4630e25fd901d4acc465a58dd8
Sarang <sarang@dopamine.fit>
2025-03-02 15:10:13 +0530

    ui updates in oraganization details view

--- lib/core/theme/color_schemes.dart
+++ lib/core/theme/color_schemes.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color(0xFF0066FF);
-  static const Color secondary = Color(0xFF00CC99);
+  static const Color secondary = Color(0x1F2563EB);
   static const Color background = Color(0xFFF5F5F5);
   static const Color textPrimary = Color(0xFF333333);
   static const Color textSecondary = Color(0xFF99A1AF);
+  static const Color textSubtle = Color(0xFF6A7282);
+  static const Color activeGreen = Color(0xFF059669);
+  static const Color lightGreen = Color(0x14059669);
   static const Color kwhite = Colors.white;
+  static const Color kblack = Colors.black;
 }
--- lib/presentation/organization_details/organization_details.dart
+++ lib/presentation/organization_details/organization_details.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tap_invest/core/constants.dart';
+import 'package:tap_invest/core/theme/color_schemes.dart';
 import 'package:tap_invest/presentation/organization_details/bloc/organization_bloc.dart';
 
 class OrganizationDetailsPage extends StatefulWidget {
@@ -27,9 +29,114 @@ class _OrganizationDetailsPageState extends State<OrganizationDetailsPage> {
         } else if (state is OrganizationError) {
           return Center(child: Text(state.message));
         } else if (state is OrganizationLoaded) {
-          return Center(
-            child:
-                Text("Page is hereeee${state.organisationDetails.companyName}"),
+          return CustomScrollView(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            slivers: [
+              SliverAppBar(
+                backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                floating: true,
+                pinned: false,
+                leading: Container(
+                  margin: EdgeInsets.only(left: 18),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: AppColors.kwhite,
+                    image: DecorationImage(
+                      image: AssetImage(
+                        "assets/images/ArrowLeft.png",
+                      ),
+                      scale: 1.6,
+                    ),
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      kheight15,
+                      Container(
+                        width: 60,
+                        height: 60,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: AppColors.kwhite,
+                          image: DecorationImage(
+                            image: NetworkImage(state.organisationDetails.logo),
+                            fit: BoxFit.contain,
+                          ),
+                          border: Border.all(
+                            color:
+                                AppColors.textSecondary.withValues(alpha: 0.2),
+                          ),
+                        ),
+                      ),
+                      kheight15,
+                      Text(
+                        state.organisationDetails.companyName,
+                        style:
+                            Theme.of(context).textTheme.displayLarge!.copyWith(
+                                  color: AppColors.kblack,
+                                  fontSize: 16,
+                                  letterSpacing: 0.5,
+                                ),
+                      ),
+                      kheight10,
+                      Text(
+                        state.organisationDetails.description,
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              fontWeight: FontWeight.normal,
+                              color: AppColors.textSubtle,
+                            ),
+                      ),
+                      kheight10,
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            height: 30,
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            alignment: Alignment.centerLeft,
+                            decoration: BoxDecoration(
+                              color: AppColors.secondary,
+                              borderRadius: BorderRadius.circular(3),
+                            ),
+                            child: Text(
+                              "ISIN: ${state.organisationDetails.isin}",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .copyWith(color: AppColors.primary),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          kwidth10,
+                          Container(
+                            height: 30,
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            alignment: Alignment.centerLeft,
+                            decoration: BoxDecoration(
+                              color: AppColors.lightGreen,
+                              borderRadius: BorderRadius.circular(3),
+                            ),
+                            child: Text(
+                              "ISIN: ${state.organisationDetails.isin}",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall!
+                                  .copyWith(color: AppColors.activeGreen),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
           );
         }
         return Center(child: Text("No data"));
